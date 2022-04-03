17bdba58316f6e9fbe409bdc12b54222f01f094f
candy2022 <96950198+candy2022@users.noreply.github.com>
2022-04-03 21:22:20 +0900

    Create User.java

---
+++ src/main/java/net/skhu/entity/User.java
@@ -0,0 +1,23 @@
+package net.skhu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id; //학번
+    String Name;
+    int phone;
+    String email;
+    String password;
+    boolean enabled;
+
+
+}
